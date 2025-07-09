aee6aff49643c8cd07c909a5134b7ef64525918a
Jose Carlos Lozano <jose.lozano29@uabc.edu.mx>
2025-07-08 18:31:08 -0700

    js changes in submit message

--- main.js
+++ main.js
@@ -31,13 +31,12 @@ const form = document.forms['submit-to-google-sheet']
 const msg = document.getElementById("msg")
   
 form.addEventListener('submit', e => {
-    e.preventDefault()
+    e.preventDefault();
     fetch(scriptURL, { method: 'POST', body: new FormData(form)}).then(response => {
-            msg.innerHTML = "Message sent successfully!"
-            setTimeout(function(){
-            msg.innerHTML = ""
-        },3000)
-        form.reset()
-    })
-    .catch(error => console.error('Error!', error.message))
+        msg.innerHTML = "Message sent successfully!";
+        setTimeout(function(){
+            msg.innerHTML = "";
+        },3000);
+        form.reset();
+    }).catch(error => console.error('Error!', error.message));
 })
